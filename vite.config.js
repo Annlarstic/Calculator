469e8d79ce14a593cffb2e412daf41ece09cd149
Annlarstic <nonsochi1823@gmail.com>
2025-04-10 10:37:33 +0100

    Update Vite config for dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/Calculator",
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.MODE === 'production' ? '/Calculator/' : '/',  // Dynamic base for production (GitHub Pages)
-});
+  base: "/Calculator",
+})
