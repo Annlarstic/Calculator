5126dd73110eb6e8e3344c52b3874e55974ee771
Annlarstic <nonsochi1823@gmail.com>
2025-04-10 10:35:42 +0100

    Fix base URL for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.MODE === 'production' ? '/Calculator/' : '/', // Use different base for production
-})
+  base: import.meta.env.MODE === 'production' ? '/Calculator/' : '/',  // Dynamic base for production (GitHub Pages)
+});
